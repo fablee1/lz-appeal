cb93f04dfdbce433dcaec4c60ca7f0f10ce487f0
Viljams Carevs <fabler65@gmail.com>
2024-05-20 16:39:45 +0300

    write results to file

--- index.ts
+++ index.ts
@@ -1,32 +1,36 @@
-import cli from "cli";
-import { Wallet } from "ethers";
-import random from "lodash/random";
-import { DELAY_SECONDS, KEYS_FILENAME, MESSAGE } from "./constants";
-import { verifySignatureRequest } from "./actions";
-import { delayProgress, loadFromFile } from "./utils";
+import cli from "cli"
+import { Wallet } from "ethers"
+import random from "lodash/random"
+import shuffle from "lodash/shuffle"
+import fs from "fs"
+import { DELAY_SECONDS, KEYS_FILENAME, MESSAGE } from "./constants"
+import { verifySignatureRequest } from "./actions"
+import { delayProgress, loadFromFile } from "./utils"
 
-const keys = await loadFromFile(KEYS_FILENAME);
+const keys = await loadFromFile(KEYS_FILENAME)
 
-for (const key of keys) {
-  const wallet = new Wallet(key);
-  const { address } = wallet;
-  console.log(`===== Адрес: ${address} ======`);
+for (const key of shuffle(keys)) {
+  const wallet = new Wallet(key)
+  const { address } = wallet
+  console.log(`===== Адрес: ${address} ======`)
 
-  const signature = await wallet.signMessage(MESSAGE);
-  console.log(`Signature Hash: ${signature}`);
+  const signature = await wallet.signMessage(MESSAGE)
+  console.log(`Signature Hash: ${signature}`)
 
   try {
-    cli.spinner("Отправляем подпись", false);
-    const result = await verifySignatureRequest(address, signature);
-    cli.spinner(result, true);
+    cli.spinner("Отправляем подпись", false)
+    const result = await verifySignatureRequest(address, signature)
+    fs.appendFileSync("result.txt", `${address}:${result}\n`)
+
+    cli.spinner(result, true)
   } catch (e) {
-    cli.spinner("", true);
-    console.log("Ошибка:", e.message);
+    cli.spinner("", true)
+    console.log("Ошибка:", e.message)
   }
 
-  const [delayFrom, delayTo] = DELAY_SECONDS;
-  const delayTimeout = random(delayFrom, delayTo);
-  await delayProgress(delayTimeout);
+  const [delayFrom, delayTo] = DELAY_SECONDS
+  const delayTimeout = random(delayFrom, delayTo)
+  await delayProgress(delayTimeout)
 
-  console.log(`=============================`);
+  console.log(`=============================`)
 }
